cb6f6cad1ac24a35f2a80ca918d28608d9a19f1b
Ryan Paglinawan <ryanpaglinawan@icloud.com>
2025-11-21 15:53:38 -0800

    Updated vercel.json

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,23 @@
 import type { NextConfig } from "next";
 
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 31536000,
+  },
+
+  compress: true,
+
   webpack: (config) => {
     return config;
   },
 };
- 
+
 export default withNextIntl(nextConfig);
